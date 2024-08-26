3d01610af17269a1471b4218887ddee798da9007
toel-kamanda34 <toelokemwa@gmail.com>
2024-08-26 12:46:49 +0300

    completed interface lab

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 function App() {
-  return <div>Your code here</div>;
+
+  return <div>
+            <h1> Hi, I'm Monalisa Sabina</h1>
+            <img src="https://via.placeholder.com/350"alt="Profile Photo" />
+            <h2>About Me</h2>
+            <p>I love react testing. it simplifies my work</p>
+            <a href="https://github.com">Github </a>
+            <a href="https://linkedin.com">linkedin </a>
+         </div>;
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,3 +4,76 @@ import '@testing-library/jest-dom';
 import App from "../App";
 
 // Your tests here
+
+test("displays a top-level heading with the text `Hi, I'm _______`", () => {
+    // Arrange
+    render(<App />);
+
+    // Act
+   const topLevelHeading = screen.getByRole("heading", {
+    name: /hi, i'm/i,
+    exact: false,
+    level: 1,
+    });
+
+    // Assert
+    expect(topLevelHeading).toBeInTheDocument();
+  });
+
+  test("displays an image of yourself with alt text identifying the content of the image", () => {
+    // Arrange
+    render(<App />);
+
+    // Act
+   const image = screen.getByAltText("Profile Photo");
+
+    // Assert
+    expect(image).toHaveAttribute("src");
+  });
+
+  test("displays a second-level heading with the text About Me", () => {
+    // Arrange
+    render(<App />);
+
+    // Act
+   const secondLevel = screen.getByRole("heading", {
+    name: /About Me/i,
+    exact: false,
+    level: 2,
+    });
+
+    // Assert
+    expect(secondLevel).toBeInTheDocument();
+  });
+
+  test("displays a paragraph for your biography", () => {
+    // Arrange
+    render(<App />);
+
+    // Act
+   const paragraph = screen.getByText(/I /i)
+
+    // Assert
+    expect(paragraph).toBeInTheDocument();
+  });
+
+  test("displays a paragraph for your biography", () => {
+    // Arrange
+    render(<App />);
+
+    // Act
+   const githubLink= screen.getByRole("link",{name:/github/i,})
+
+   const linkedinLink = screen.getByRole("link",{name:/linked/i})
+
+    // Assert
+    expect(githubLink).toHaveAttribute(
+        "href",
+        expect.stringContaining("https://github.com")
+      );
+
+      expect(linkedinLink).toHaveAttribute(
+        "href",
+        expect.stringContaining("https://linkedin.com")
+      );
+  });
